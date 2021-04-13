7e73e1a6e6b33781ab9f86d9da0afafd6d737960
Gints <gints.ozolins@inbox.lv>
2021-04-13 15:07:43 +0300

    Final version

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     ],
     darkMode: false,
     theme: {
-        extend: {},
+        extend: {
+            spacing: {
+                '22': '5.5rem',
+                '30': '7.5rem',
+                '50': '12.5rem',
+                '76': '19rem',
+            }
+        },
+        screens: {
+            'xs': '360px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        },
     },
     variants: {
         extend: {},
